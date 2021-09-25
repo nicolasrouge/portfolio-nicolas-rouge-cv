e702cdca7e2b52eceeda384aa70745b43cf6ab6a
Nicolas R <nicolas.rouge777@gmail.com>
2021-09-25 23:42:15 +0100

    few fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Nicolas Rouge Portfolio`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Nicolas Rouge Portfolio`,
+        short_name: `Nick Portfolio`,
+        description: `This is the portfolio of a Full-Stack Developer C# and React called Nick`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
