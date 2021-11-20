ab0aecf33cd3ee75ca5492f27849106cd34caeee
Nicolas R <nicolas.rouge777@gmail.com>
2021-11-20 17:09:56 +0000

    project added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,18 @@ import { Parallax } from "@react-spring/parallax"
 import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
 import Hero from "@lekoarts/gatsby-theme-cara/src/components/hero"
 import Projects from "@lekoarts/gatsby-theme-cara/src/components/projects"
-
+import Helmet from "react-helmet"
 import About from "@lekoarts/gatsby-theme-cara/src/components/about"
-
 import Contact from "@lekoarts/gatsby-theme-cara/src/components/contact"
+
 const Cara = () => (
   <Layout>
+    <Helmet>
+    <script>var stylesheet = "https://app.testdome.com/content/source/stylesheets/embed.css", link = document.createElement("link"); link.href = stylesheet, link.type = "text/css", link.rel = "stylesheet", link.media = "screen,print", document.getElementsByTagName("head")[0].appendChild(link);</script>
+
+      </Helmet>
     <Parallax pages={5}>
+      
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={2} />
       <About offset={3} factor={1} />
